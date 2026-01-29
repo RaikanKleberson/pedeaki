b62e0a867d1ba3f67adfdbf6de70192497190fa5
Raikan Kleerson <raikankleberson@gmail.com>
2026-01-29 11:49:04 -0300

    Otimização das imagens dos Produtod

--- src/js/script.js
+++ src/js/script.js
@@ -19,28 +19,28 @@ const produtos = [
     nome: "Açúcar 1kg", 
     preco: 4.99, 
     qtd: 0,
-    imagem: "src/images/produtos/acucar.jpg"
+    imagem: "src/images/produtos/acucar.png"
   },
   { 
     id: 4, 
     nome: "Óleo 900ml", 
     preco: 7.90, 
     qtd: 0,
-    imagem: "src/images/produtos/oleo.jpg"
+    imagem: "src/images/produtos/oleo.png"
   },
   { 
     id: 5, 
     nome: "Café 500g", 
     preco: 12.90, 
     qtd: 0,
-    imagem: "src/images/produtos/cafe.jpg"
+    imagem: "src/images/produtos/cafe.png"
   },
   { 
     id: 6, 
     nome: "Leite 1L", 
     preco: 4.50, 
     qtd: 0,
-    imagem: "src/images/produtos/leite.jpg"
+    imagem: "src/images/produtos/leite.png"
   }
 ];
 
